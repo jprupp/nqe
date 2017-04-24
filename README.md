6fb4f9806e27b1f8e1246785c31b9c4a98a2bfc6
Jean-Pierre Rupp <jp@blockchain.com>
2017-04-24 16:07:46 +0100

    Add some unit tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# nqe
+# Not Quite Erlang
+
+Haskell framework for simple concurrent processes with mailboxes inspired by Erlang/OTP.
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
+import Control.Monad
+import Data.Typeable
+import Test.Hspec
+import Control.Concurrent.NQE
+
+data Ping = Ping deriving (Eq, Show, Typeable)
+data Pong = Pong deriving (Eq, Show, Typeable)
+
+ping :: Process -> Process -> IO ()
+ping proc my = replicateM_ 4 $ do
+    send proc (my, Ping)
+    Pong <- receive
+    return ()
+
+pong :: Process -> IO ()
+pong _ = forever $ do
+    (proc, Ping) <- receive
+    send proc Pong
+
+usingWithProcess :: IO ()
+usingWithProcess =
+    withProcess "pong" pong $ \ po ->
+    withProcess "ping" (ping po) $ \ pi ->
+    waitFor pi
+
+usingStartProcess :: IO ()
+usingStartProcess = do
+    po <- startProcess "pong" pong
+    pi <- startProcess "ping" (ping po)
+    waitFor pi
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "two processses exchange ping/pong messages" $ do
+        it "launched via startProcess" $
+            usingStartProcess
+        it "launched via withProcess" $
+            usingWithProcess
