3bc919ad8f262420e05b04e9ad1828c471af68b6
Jean-Pierre Rupp <xenog@protonmail.com>
2017-06-13 18:47:00 +0100

    Add network and update LTS Haskell

--- src/Control/Concurrent/NQE.hs
+++ src/Control/Concurrent/NQE.hs
@@ -1,5 +1,7 @@
 module Control.Concurrent.NQE
     ( module Control.Concurrent.NQE.Process
+    , module Control.Concurrent.NQE.Network
     ) where
 
-import Control.Concurrent.NQE.Process
+import           Control.Concurrent.NQE.Network
+import           Control.Concurrent.NQE.Process
