321499dbd1247ad20da49132a2f5896f6f52ee7f
Jean-Pierre Rupp <xenog@protonmail.com>
2017-12-23 19:05:39 +0000

    Export lifted-async module

--- src/Control/Concurrent/NQE.hs
+++ src/Control/Concurrent/NQE.hs
@@ -3,10 +3,10 @@ module Control.Concurrent.NQE
     , module Control.Concurrent.NQE.Network
     , module Control.Concurrent.NQE.Supervisor
     , module Control.Concurrent.STM
-    , module Control.Concurrent.Async
+    , module Control.Concurrent.Async.Lifted
     ) where
 
-import           Control.Concurrent.Async
+import           Control.Concurrent.Async.Lifted
 import           Control.Concurrent.NQE.Network
 import           Control.Concurrent.NQE.Process
 import           Control.Concurrent.NQE.Supervisor
